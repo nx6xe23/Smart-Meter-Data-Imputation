1a803d0e096df0e080459a14e65bc96ff83ccbb4
a-saraf <a-saraf@github.com>
2024-02-23 20:20:43 +0530

    Data Analysis

--- README.md
+++ README.md
@@ -15,3 +15,17 @@ python data_cleaning_london.py
 This will go through the csv files and cluster the data for each household and save it in the data folder. It will also go through the data and add `NaN` values to the data with missing readings. 
 
 ---
+## Data Analysis
+
+Run the following python script to analyze the dataset. 
+
+```
+python data_analysis.py
+```
+
+For analyzing the dataset, first we find which of the households have no missing data points. This is saved in `complete_households.txt` file in the `data/london_data/` folder. The percentage of missing values in the non complete households is calculated. Furthermore, the ratio of random and clustered missing data values is calculated. The mean and the variance of the cluster sizes is also calculated. The mean is calculated using $\bar{x} = \cfrac{\sum_i^n x_i}{n}$ and the variance is $\sigma^2 = \cfrac{\sum(x_i - \bar{x})^2}{n-1}$. 
+
+
+The average missing data is 0.38% for the non-complete households, of which 21.72% and 78.28% are random and cluster missing data. The average length of a cluster is 52.88 and the variance is 58827.56. 
+
+---
---
+++ data_analysis.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from tqdm import tqdm 
+import os
+
+os.chdir('./data/london_data/')
+write_file = open('complete_households.txt', 'w')
+
+list_addr = sorted(os.listdir('./'))[:-1]
+missing_files = []
+
+missing, total = 0, 0
+
+for i in tqdm(list_addr):
+    file = pd.read_csv(i)['KWh']
+    if file.isnull().values.any() == True:
+        missing += file.isnull().values.sum()
+        total += file.size
+        missing_files.append(i)
+        continue
+    write_file.write(i + '\n')
+
+write_file.close()
+
+print('Average Missing Data:', f'{(missing/total*100):.2f}%')
+
+random, cluster = 0, 0
+cluster_len = 0
+cluster_sizes = []
+
+for i in tqdm(missing_files):
+    df = pd.read_csv(i)
+    missing_idx = df[df['KWh'].isnull()].index.tolist()
+    cluster_len = 0
+
+    for j in range(len(missing_idx)):
+        if j == len(missing_idx) - 1:
+            if missing_idx[j] - missing_idx[j-1] > 1:
+                random += 1
+                if cluster_len != 0:
+                    cluster_sizes.append(cluster_len)
+                cluster_len = 0
+            else:
+                cluster += 1
+                cluster_len += 1
+                cluster_sizes.append(cluster_len)
+
+        elif missing_idx[j+1] - missing_idx[j] > 1:
+            random += 1
+            if cluster_len != 0:
+                cluster_sizes.append(cluster_len)
+            cluster_len = 0
+        else:
+            cluster += 1
+            cluster_len += 1
+
+print('Random Missing Data:', f'{(random/(random+cluster)*100):.2f}%')
+print('Cluster Missing Data:', f'{(cluster/(random+cluster)*100):.2f}%')
+
+mean = sum(cluster_sizes) / len(cluster_sizes) 
+var = sum((i - mean) ** 2 for i in cluster_sizes) / (len(cluster_sizes) - 1)
+
+print('Average Cluster Size:', f'{mean:.2f}')
+print('Variance of Cluster Size:', f'{var:.2f}')
